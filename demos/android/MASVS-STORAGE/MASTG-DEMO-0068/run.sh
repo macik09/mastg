2e1c845f1d4c155754f610e9d1dbbac777bff7e4
Maciej Duda <64193188+macik09@users.noreply.github.com>
2025-11-26 10:11:51 +0100

    Update demos/android/MASVS-STORAGE/MASTG-DEMO-0068/run.sh
    Co-authored-by: Carlos Holguera <perezholguera@gmail.com>

--- demos/android/MASVS-STORAGE/MASTG-DEMO-0068/run.sh
+++ demos/android/MASVS-STORAGE/MASTG-DEMO-0068/run.sh
@@ -7,15 +7,11 @@ SDCARD_PULL_PATH="/sdcard/$DB_NAME.db"
 OUTPUT_DB_FILE="${DB_NAME}.db"
 OUTPUT_TXT_FILE="output.txt"
 
-# Step 1: Database Extraction
-adb shell "run-as $PACKAGE cp databases/$DB_NAME $SDCARD_PULL_PATH" 2>/dev/null
-
-# Attempt to copy via root access using here document
-adb shell <<EOF
-su
-cp $DB_PATH $SDCARD_PULL_PATH
-exit
-EOF
+# Copy Database to the sdcard
+if ! adb shell "run-as $PACKAGE cp databases/$DB_NAME $SDCARD_PULL_PATH"; then
+  echo "run-as failed, trying su"
+  adb shell "su 0 sh -c 'cp $DB_PATH $SDCARD_PULL_PATH'"
+fi
 
 # Pull the file to the host machine
 adb pull "$SDCARD_PULL_PATH" .
